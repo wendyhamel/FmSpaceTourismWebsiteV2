bacd3e29bd6ed6bddecd134bd5e0c3fc88dc1c51
wendyhamel <whamel@it4web.nl>
2024-11-18 18:08:16 +0100

    working on setup for layout

---
+++ src/site.js
@@ -0,0 +1,28 @@
+window.showContent = function () {
+	return {
+		menu: [
+			{
+				number: '00',
+				title: 'Home',
+				href: "#"
+			},
+			{
+				number: '01',
+				title: 'Destination',
+				href: "#"
+			},
+			{
+				number: '02',
+				title: 'Crew',
+				href: "#"
+			},
+			{
+				number: '03',
+				title: 'Technology',
+				href: "#"
+			},
+		],
+		mobileMnuOpened: false,
+
+	}
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,48 @@ module.exports = {
   theme: {
     extend: {
         fontFamily: {
-          'sans': ['Bellefair', 'sans-serif'],
+          'heading': ['Bellefair', 'sans-serif'], // preset 1 2 3 4 6
+          'sans': ['Barlow\\ Condensed', 'sans-serif'], // preset 5 7 8 9
         },
+        colors: {
+            'space-blue': {
+                dark: 'rgb(11 13 23)', // blue 900
+                light: 'rgb(208 214 249)' // blue 300
+            }
+        },
+        fontSize: {
+            'space-4xl': '9rem', // preset 1
+            'space-3xl': '6.25rem', // preset 2
+            'space-2xl': '3.5rem', // preset 3
+            'space-xl': '2rem', // preset 4
+            'space-lg': '1.75rem', // preset 5 & preset 6
+            'space-md': '1.125rem', // preset 9
+            'space-sm': '1rem', // preset 8
+            'space-xs': '0.875rem', // preset 7
+        },
+        letterSpacing: {
+            'space-lg': '0.125em', // preset 5 & preset 8
+            'space-xl': '0.143em', // preset 7
+        },
+        lineHeight: {
+            'space': '2.025rem', // preset 9
+        },
+        spacing: {
+            2: '0.5rem', // for 2 use 0.tw class -> 0.5
+            4: '1rem', // for 4 use tw class -> 1
+            8: '2rem', // for 8 use tw class -> 2
+            12: '3rem', // for 12 use tw class -> 3
+            16: '4rem', // for 16 use tw class -> 4
+            24: '6rem', // for 24 use tw class -> 6
+            32: '8rem', // for 32 use tw class -> 8
+            40: '10rem', // for 40 use tw class -> 10
+            48: '12rem', // for 48 use tw class -> 12
+            64: '16rem', // for 64 use tw class -> 16
+            80: '20rem', // for 80 use tw class -> 20
+            96: '24rem', // for 96 use tw class -> 24
+            128: '32rem', // for 128 use tw class -> 32
+        }
     },
   },
-  plugins: [],
 }
 
