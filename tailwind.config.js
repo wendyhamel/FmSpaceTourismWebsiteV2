2e60e2af39324ba1b6b3169992ea5ff075231f0b
wendyhamel <whamel@it4web.nl>
2024-11-21 16:41:05 +0100

    Setup pages with start content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  content: ['./index.html', './destination.html', './destination/index.html'],
   theme: {
     extend: {
         fontFamily: {
