03111941958d2fbb631dc819871f14adbb44ddf0
wendyhamel <whamel@it4web.nl>
2024-11-22 15:20:31 +0100

    js for active tabs

--- src/site.js
+++ src/site.js
@@ -22,8 +22,6 @@ window.showContent = function () {
 				href: "/FmSpaceTourismWebsiteV2/technology"
 			},
 		],
-		mobileMenuOpened: false,
-		currentPage: Alpine.$persist('Home'),
 		destinations: [
 			{
 				name: "Moon",
@@ -129,6 +127,11 @@ window.showContent = function () {
 				},
 				description: "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained."
 			}
-		]
+		],
+		mobileMenuOpened: false,
+		currentPage: Alpine.$persist('Home'),
+		activeDestination: 0,
+		activeCrew: 0,
+		activeTechnology: 0,
 	}
 }
