92d56a8b45f4964d022f1fa663d4c4436f59870b
wendyhamel <whamel@it4web.nl>
2024-11-29 12:21:33 +0100

    Clean up helpers

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,42 +4,40 @@ module.exports = {
   theme: {
     extend: {
         fontFamily: {
-          'heading': ['Bellefair', 'sans-serif'], // preset 1 2 3 4 6
-          'sans': ['Barlow\\ Condensed', 'sans-serif'], // preset 5 7 8 9
+          'heading': ['Bellefair', 'sans-serif'],
+          'sans': ['Barlow\\ Condensed', 'sans-serif'],
         },
         colors: {
             'space-blue': {
-                dark: 'rgb(11 13 23)', // blue 900
-                light: 'rgb(208 214 249)' // blue 300
+                dark: 'rgb(11 13 23)',
+                light: 'rgb(208 214 249)'
             }
         },
         fontSize: {
-            'space-4xl': '9rem', // preset 1
-            'space-4xl-res': 'min(max(5.125rem,20vw),9rem)', // preset 1 responsive
-            'space-3xl': '6.25rem', // preset 2
-            'space-3xl-res': 'min(max(3rem,15vw),6.25rem)', // preset 2 responsive
-            'space-2xl': '3.5rem', // preset 3
-            'space-xl': '2rem', // preset 4
-            'space-xl-res': 'min(max(1.125rem,4vw),2rem)', // preset 4 responsive
-            'space-lg': '1.75rem', // preset 5 & preset 6
-            'space-lg-res': 'min(max(1rem,4vw),1.75rem)', // preset 5 & preset 6 responsive
-            'space-md': '1.2rem', // preset 9
-            'space-md-res': 'min(max(1rem,4.8vw),1.2rem)', // preset 9 responsive
-            'space-sm': '1rem', // preset 8
-            'space-xs': '0.875rem', // preset 7
+            'space-4xl-res': 'min(max(5.125rem,20vw),9rem)',
+            'space-3xl': '6.25rem',
+            'space-2xl': '3.5rem',
+            'space-xl': '2rem',
+            'space-xl-res': 'min(max(1.125rem,4vw),2rem)',
+            'space-lg': '1.75rem',
+            'space-lg-res': 'min(max(1rem,4vw),1.75rem)',
+            'space-md': '1.2rem',
+            'space-md-res': 'min(max(1rem,4.8vw),1.2rem)',
+            'space-sm': '1rem',
+            'space-xs': '0.875rem',
         },
         letterSpacing: {
             'space-md': '0.06em',
-            'space-lg': '0.125em', // preset 5 & preset 8
-            'space-xl': '0.143em', // preset 7
+            'space-lg': '0.125em',
+            'space-xl': '0.143em',
             'space-2xl': '0.175em'
         },
         lineHeight: {
-            'space': '2.025rem', // preset 9
+            'space': '2.025rem',
         },
         spacing: {
-            '30': '7.25rem;', //116px
-            '68': '17rem;', // 272px
+            '30': '7.25rem;',
+            '68': '17rem;',
         },
         borderWidth: {
             '3': '3px'
